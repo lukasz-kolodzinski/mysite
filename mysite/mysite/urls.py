0c4999d1110566204195e03a747909b0402ed2c1
Łukasz Kołodziński <luk.kolodzinski@gmail.com>
2018-03-17 12:09:45 +0100

    new view:current_datetime added and applied;TIME_ZONE changed

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from views import current_datetime
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    url(r'^time/$', current_datetime),
 ]
